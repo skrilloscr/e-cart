c7e50cda3688bc693cf12c8283b81b526dde26ea
skrilloscr <midhun.k.manoj1290@gmail.com>
2023-10-25 11:43:58 +0530

    second commit

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { emteyCart, removeFromCart } from '../Redux/cartSlice'
 
 function Cart() {
